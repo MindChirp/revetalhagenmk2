a0292c183b11c97036a11ef9946d39b2497b4c3e
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2025-04-14 17:40:45 +0200

    Fixed animations

--- src/app/arrangementer/page.tsx
+++ src/app/arrangementer/page.tsx
@@ -1,23 +1,25 @@
 import SlideAnimation from "@/components/ui/animated/slide-animation";
+import CreateNews from "@/components/ui/create-news";
 import EventCalendar from "@/components/ui/event-calendar";
 
-function Page() {
+async function Page() {
   return (
     <div className="relative flex flex-col items-center gap-5 pt-36">
       <h1 className="text-foreground flex flex-row gap-5 pl-20 text-6xl leading-20">
         Hva
         <SlideAnimation
           direction="right"
+          className="text-secondary-foreground bg-secondary overflow-hidden rounded-3xl px-2"
           transition={{
-            delay: 0.7,
-            duration: 0.5,
+            delay: 0.4,
+            duration: 0.3,
           }}
         >
           <SlideAnimation
             direction="up"
-            className="text-secondary-foreground bg-secondary overflow-hidden rounded-3xl px-2"
+            className=""
             transition={{
-              delay: 1.2,
+              delay: 0.7,
             }}
           >
             skjer
@@ -25,7 +27,8 @@ function Page() {
         </SlideAnimation>
         i Revetalhagen?
       </h1>
-      <div className="relative flex w-full flex-0 flex-col items-center pb-10">
+      <div className="relative flex w-full flex-0 flex-col items-center gap-2.5 pb-10">
+        <CreateNews />
         <EventCalendar />
       </div>
     </div>
